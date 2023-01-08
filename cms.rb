0d976e635a475131963ddb261975a2f41f4c6f14
Brandi Seeley <brandi.n.seeley@gmail.com>
2023-01-08 15:49:58 -0700

    Ability to changeAbility to edit documents added

--- cms.rb
+++ cms.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'redcarpet'
+require 'pathname'
 
 configure do
   enable :sessions
@@ -12,6 +14,27 @@ before do
   @file_paths = Dir.glob(root + "/data/*")
 end
 
+def suffix(filename)
+  point_index = filename.index(".")
+  filename[point_index..-1]
+end
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/html"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
+
 get "/" do
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
@@ -21,13 +44,28 @@ end
 
 get "/:filename" do
   file_path = root + "/data/" + params[:filename]
-
+  
   if File::file?(file_path)
-    headers["Content-Type"] = "text/html"
-    @file_contents = File.readlines(file_path)
+    @file_content = load_file_content(file_path)
     erb :file
   else
-    session[:error] = "#{params[:filename]} does not exist."
+    session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
   end
+end
+
+get "/:filename/edit" do
+  @filename = params[:filename]
+  @file_path = root + "/data/" + @filename
+  @file_content = load_file_content(@file_path)
+  erb :edit_file
+end
+
+post "/:filename" do
+  file_path = root + "/data/" + params[:filename]
+
+  File.write(file_path, params[:new_content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
 end
---
+++ data/samplemarkdown.md
@@ -0,0 +1,11 @@
+# Markdown! #
+## A very helpful tool ##
+### I love it. ###
+```WOW::YouCanEvenWrite(code, snippets: cool)
+```
+#### Another thing! ####
+- you can make lists
+- that make bullet points
+- wow
+
+***And a lot of other cool things***
