4241a1fcdcb529fd355d69d4551e374194fedb2f
Brandi Seeley <brandi.n.seeley@gmail.com>
2023-01-09 14:27:50 -0700

    Require login for change/delete features

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -34,21 +34,25 @@ class CMSTest < Minitest::Test
     last_request.env["rack.session"]
   end
 
-  def test_index
-    get "/signin", username: "admin", password: "secret"
+  def admin_session
+    { "rack.session" => { user: "admint" } }
+  end
 
+  def test_index
+    get "/signin", {}, admin_session
+    
     create_document "about.md"
     create_document "changes.txt"
-
+    
     get "/"
 
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "about.md"
     assert_includes last_response.body, "changes.txt"
-
+    
   end
-
+  
   def test_viewing_text_document
     create_document "history.txt", "Ruby 0.95 released"
     
@@ -58,35 +62,36 @@ class CMSTest < Minitest::Test
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "Ruby 0.95 released"
   end
-
+  
   def test_viewing_markdown_document
     create_document "about.md", "# Ruby is..."
-
+    
     get "/about.md"
-
+    
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
-
+  
   def test_document_not_found
     get "/notafile.ext"
-
+    
     assert_equal 302, last_response.status
     assert_equal "notafile.ext does not exist.", session[:message]
   end
-
+  
   def test_editing_document
     create_document "changes.txt"
-
-    get "/changes.txt/edit"
-
+    get "/changes.txt/edit", {}, { "rack.session" => { user: "admin"} }
+    
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<textarea"
     assert_includes last_response.body, %q(<button type="submit")
   end
   
   def test_updating_document
+    get "/signin", {}, admin_session
+
     post "/changes.txt", content: "new content"
 
     assert_equal 302, last_response.status
@@ -100,7 +105,7 @@ class CMSTest < Minitest::Test
   end
   
   def test_view_new_document_form
-    get "/new"
+    get "/new", {}, admin_session
 
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<input"
@@ -108,7 +113,7 @@ class CMSTest < Minitest::Test
   end
 
   def test_create_new_document
-    get "/signin", username: "admin", password: "secret"
+    get "/signin", {}, admin_session
     
     post "/create", filename: "test.txt"
     assert_equal 302, last_response.status
@@ -121,7 +126,7 @@ class CMSTest < Minitest::Test
   end
 
   def test_create_new_document_without_filename
-    post "/create", filename: ""
+    post "/create", { filename: "" }, { "rack.session" => { user: "admin"} }
     assert_equal 422, last_response.status
     assert_includes last_response.body, "A name is required"
   end
@@ -129,7 +134,7 @@ class CMSTest < Minitest::Test
   def test_deleting_document
     create_document("test.txt")
 
-    post "/test.txt/delete"
+    post "/test.txt/delete", {}, { "rack.session" => { user: "admin"} }
 
     assert_equal 302, last_response.status
     
@@ -140,40 +145,67 @@ class CMSTest < Minitest::Test
     refute_includes last_response.body, "test.txt"
   end
 
-def test_signin_form
-  get "/"
+  def test_signin_form
+    get "/", {}, { "rack.session" => { user: "admin"} }
 
-  assert_equal 200, last_response.status
-  assert_includes last_response.body, "<input"
-  assert_includes last_response.body, %q(<input type="submit")
-end
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<input type="submit")
+  end
 
-def test_signin
-  get "/signin", username: "admin", password: "secret"
-  assert_equal 302, last_response.status
-  assert_equal "Welcome!", session[:message]
-  assert_equal "admin", session[:user]
+  def test_signin
+    get "/signin", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+    assert_equal "Welcome!", session[:message]
+    assert_equal "admin", session[:user]
 
-  get last_response["Location"]
-  assert_includes last_response.body, "Signed in as admin"
-end
+    get last_response["Location"]
+    assert_includes last_response.body, "Signed in as admin"
+  end
 
-def test_signin_with_bad_credentials
-  get "/signin", username: "guest", password: "shhhh"
-  assert_equal 422, last_response.status
-  assert_nil session[:username]
-  assert_includes last_response.body, "Invalid credentials"
-end
+  def test_signin_with_bad_credentials
+    get "/signin", username: "guest", password: "shhhh"
+    assert_equal 422, last_response.status
+    assert_nil session[:username]
+    assert_includes last_response.body, "Invalid credentials"
+  end
 
-def test_signout
-  get "/", {}, {"rack.session" => { user: "admin" } }
-  assert_includes last_response.body, "Signed in as admin"
+  def test_signout
+    get "/", {}, {"rack.session" => { user: "admin" } }
+    assert_includes last_response.body, "Signed in as admin"
 
-  get "/signout"
-  assert_equal "You have been signed out.", session[:message]
+    get "/signout"
+    assert_equal "You have been signed out.", session[:message]
 
-  get last_response["Location"]
-  assert_nil session[:username]
-  assert_includes last_response.body, "Sign In"
+    get last_response["Location"]
+    assert_nil session[:username]
+    assert_includes last_response.body, "Sign In"
+  end
+
+  def test_visit_edit_page_signed_out
+    get "/:filename/edit"
+    
+    assert_equal 302, last_response.status
+    assert_equal "You must be signed in to do that.", session[:message]
+    end
+    
+    def test_submit_changes_signed_out
+      post "/:filename"
+      
+      assert_equal 302, last_response.status
+      assert_equal "You must be signed in to do that.", session[:message]
+  end
+
+  def test_visit_new_document_page_signed_out
+
+  end
+
+  def test_submit_new_document_signed_out
+    
+  end
+
+  def test_delete_document_signed_out
+
   end
 end
+
